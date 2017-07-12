90d2c4afce5c7b49cce32e6c146394fde2347349
SeanCoder <seandscott@me.com>
2017-07-12 16:46:55 -0700

    Expand the XCLIENT command so we set the session properties based on the details passed in.

--- src/main/java/org/subethamail/smtp/command/XclientCommand.java
+++ src/main/java/org/subethamail/smtp/command/XclientCommand.java
@@ -3,8 +3,11 @@ package org.subethamail.smtp.command;
 import org.subethamail.smtp.DropConnectionException;
 import org.subethamail.smtp.server.BaseCommand;
 import org.subethamail.smtp.server.Session;
+import org.subethamail.smtp.util.Base64;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 import java.util.StringTokenizer;
 
@@ -21,9 +24,8 @@ public class XclientCommand extends BaseCommand {
 
     @Override
     public void execute(String commandString, Session sess) throws IOException, DropConnectionException {
-        if (sess.isMailTransactionInProgress())
-        {
-            sess.sendResponse("503 5.5.1 Sender already specified.");
+        if (sess.isMailTransactionInProgress()) {
+            sess.sendResponse("503 Mail transaction in progress.");
             return;
         } else if (commandString.trim().equals("XCLIENT")) {
             sess.sendResponse("501 Missing parameters.");
@@ -35,33 +37,117 @@ public class XclientCommand extends BaseCommand {
 
         while(tokens.hasMoreTokens()) {
             String parameter = tokens.nextToken();
-            StringTokenizer parameterTokenizer = new StringTokenizer(parameter, "=");
-            if (parameterTokenizer.countTokens() != 2) {
+            List<String> parameterValuePair = this.extractParameterNameValuePair(parameter, "=");
+            if (parameterValuePair == null || parameterValuePair.size() != 2) {
                 sess.sendResponse("501 Parameter format error, must be PARAM=VALUE.");
                 return;
             }
-            String parameterName = parameterTokenizer.nextToken().toUpperCase(Locale.ENGLISH);
-            String parameterValue = parameterTokenizer.nextToken();
+            String parameterName = parameterValuePair.get(0).toUpperCase(Locale.ENGLISH);
+            String parameterValue = parameterValuePair.get(1);
+            if (parameterValue.isEmpty()) {
+                sess.sendResponse("501 Parameter format error, must be PARAM=VALUE.");
+                return;
+            }
+            boolean paramaterTypeUnavailable = isUnavailable(parameterValue);
+            // Decode the parameter name and set the parameter value correctly
             if (parameterName.equals("NAME")) {
-                // TODO ignore or add this to he session as this is not used
+                if (!paramaterTypeUnavailable) { 
+                    sess.setRemoteClientName(parameterName);
+                } else {
+                    sess.setRemoteClientName(null);
+                }
             } else if (parameterName.equals("ADDR")) {
-                // TODO ignore or add this to he session as this is not used
+                if (!paramaterTypeUnavailable) { 
+                    sess.setRemoteClientIP(parameterValue);
+                } else {
+                    sess.setRemoteClientIP(null);
+                }
             } else if (parameterName.equals("PORT")) {
-                // TODO ignore or add this to he session as this is not used
+                if (!paramaterTypeUnavailable) { 
+                    sess.setRemoteClientPort(Integer.parseInt(parameter, 10));
+                } else {
+                    sess.setRemoteClientPort(null);
+                }
             } else if (parameterName.equals("PROTO")) {
-                // TODO ignore or add this to he session as this is not used
+                // TODO add support for this
             } else if (parameterName.equals("HELO")) {
-                // TODO ignore or add this to he session as this is not used
+                if (!paramaterTypeUnavailable) { 
+                    sess.setHelo(parameterValue);
+                }
             } else if (parameterName.equals("LOGIN")) {
-                // TODO ignore or add this to he session as this is not used
+                String userName = null;
+                String password = null;
+                if (!paramaterTypeUnavailable) { 
+                    List<String> userPasswordPair = this.extractParameterNameValuePair(parameterValue, ":");
+                    if (userPasswordPair != null && userPasswordPair.size() > 0 && userPasswordPair.size() <= 2) {
+                        userName = userPasswordPair.get(0);
+                        if (isUnavailable(userName)) {
+                            userName = null;
+                        }
+                        if (userPasswordPair.size() > 1) {
+                            password = userPasswordPair.get(1);
+                            if (isUnavailable(password)) {
+                                password = null;
+                            }
+                        }
+                    } else {
+                        sess.sendResponse("501 Parameter format error, must be LOGIN=<user alias>:<base 64 encoded password>.");
+                        return;
+                    }
+                }
+                sess.setUsername(userName);
+                sess.setPassword(password);
             } else if (parameterName.equals("DESTADDR")) {
-                // TODO ignore or add this to he session as this is not used
+                if (!paramaterTypeUnavailable) { 
+                    sess.setDestinationIP(parameterValue);
+                } else {
+                    sess.setDestinationIP(null);
+                }
             } else if (parameterName.equals("DESTPORT")) {
-                // TODO ignore or add this to he session as this is not used
+                if (!paramaterTypeUnavailable) { 
+                    sess.setDestinationPort(Integer.parseInt(parameterValue, 10));
+                } else {
+                    sess.setDestinationPort(null);
+                }
             } else {
                 sess.sendResponse(String.format("501 Parameter %s not supported.", parameterName));
                 return;
             }
         }
+
+        sess.sendResponse("220 ok");
+    }
+
+    private List<String> extractParameterNameValuePair(String token, String delimiter) {
+        if (token == null || token.isEmpty()) {
+            return null;
+        }
+        List<String> pairList = new ArrayList<String>();
+        int delimiterPosition = token.indexOf(delimiter);
+        if (delimiterPosition >= 0) {
+            // Extract the first part on the left side
+            if (delimiterPosition > 0) {
+                pairList.add(token.substring(0, delimiterPosition - 1).trim());
+            } else {
+                pairList.add("");
+            }
+            // extract the right side
+            if (delimiterPosition < (token.length() - 1)) {
+                pairList.add(token.substring(delimiterPosition + 1).trim());
+            } else {
+                pairList.add("");
+            }
+
+            return pairList;
+        } else {
+            pairList.add(token.trim());
+        }
+
+        return pairList;
+    }
+
+    private boolean isUnavailable(String value) {
+        return value == null || value.isEmpty() || value.equalsIgnoreCase("[UNAVAILABLE]") ||
+                value.equalsIgnoreCase("[TEMPUNAVAIL]");
     }
 }
