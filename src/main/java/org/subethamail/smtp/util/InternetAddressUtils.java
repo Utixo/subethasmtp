15ceba630daa58c5bb459b4e9276426040c81dbe
SeanCoder <seandscott@me.com>
2017-07-13 13:43:16 -0700

    Add Internet address utils that works similar to the email to veify the address value

---
+++ src/main/java/org/subethamail/smtp/util/InternetAddressUtils.java
@@ -0,0 +1,34 @@
+package org.subethamail.smtp.util;
+
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+
+/**
+ * @author Sean Scott
+ */
+public class InternetAddressUtils
+{
+	/**
+	 * @return true if the string is a valid email address
+	 */
+	public static boolean isValidAddress(String address)
+	{
+		if (address == null || address.length() == 0)
+			return true;
+
+		boolean result = false;
+		try
+		{
+			InternetAddress[] ia = InternetAddress.parse(address, true);
+			if (ia.length == 0)
+				result = false;
+			else
+				result = true;
+		}
+		catch (AddressException ae)
+		{
+			result = false;
+		}
+		return result;
+	}
+}
